a34337fa4991da38d578f9ed5ca518369efc5b96
csherms <casey.sherman@bryanuniversity.edu>
2021-02-18 16:20:09 -0700

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+function App() {
+    return (
+        <div>
+            <input type="checkbox" />
+            <p>Wake up</p>
+            
+            <input type="checkbox" />
+            <p>Get dressed</p>
+            
+            <input type="checkbox" />
+            <p>Do Workout</p>
+            
+            <input type="checkbox" />
+            <p>Take vitamins</p>
+        </div>
+    )
+}
+
+export default App
