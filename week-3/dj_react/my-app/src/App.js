3372ed813e7c9678077d2959c7b068e171c9a63e
csherms <casey.sherman@bryanuniversity.edu>
2021-03-08 11:15:42 -0700

    first commit

---
+++ week-3/dj_react/my-app/src/App.js
@@ -0,0 +1,14 @@
+  
+import React from 'react';
+import './App.css';
+import DjReact from './components/DjReact';
+
+function App() {
+  return (
+    <div className="App">
+      <DjReact />
+    </div>
+  );
+}
+
+export default App;
