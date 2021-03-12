781914428c42d7851cca2c6e5cad54ef0a7d5868
csherms <casey.sherman@bryanuniversity.edu>
2021-03-12 10:58:25 -0700

    first commit

---
+++ week-4/name_entry/my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import NameForm from './name-entries/nameForm/NameForm';
+
+function App() {
+  return (
+    <div>
+      <NameForm />
+    </div>
+  );
+}
+
+export default App;
